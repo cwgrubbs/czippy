e3af334fe5cffaf3645ad967c30850c521a38b28
cgrubbs <cgrubbs@leateam.com>
2024-04-10 10:39:49 -0400

    adding separate endpoint to test api

--- flask-docker/app.py
+++ flask-docker/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from spleeter_separator import SpleeterSeparator
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Docker</h1>'
+
+
+@app.route('/separate')
+def separate():
+    separator = SpleeterSeparator
 
 
 if __name__ == "__main__":
